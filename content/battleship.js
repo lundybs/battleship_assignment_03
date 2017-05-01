4b433a04d433173400adf9ce6dc853b0227309b8
Brad Lundy <crusin67@hotmail.com>
2017-04-30 17:27:50 -0700

    initial upload

---
+++ content/battleship.js
@@ -0,0 +1,31 @@
+var guesses = 0;
+var hits = 0;
+var isSunk = false;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
+
+while (isSunk == false) {
+    guess = prompt("Read, aim, fire! (enter a number from 0-6):");
+    if (guess < 0 || guess > 6) {
+        alert("please enter a valid cell number!");
+    }
+    else {
+        guesses = guesses + 1
+        if (guess == location1 || guess == location2 || guess == location3) {
+            alert("Hit!");
+            hits = hits + 1;
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my battleship!");
+            }
+        }
+        else {
+            alert("Miss!")
+        }
+    }
+}
+
+var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting acuracy was " + (3 / guesses);
+alert(stats);
